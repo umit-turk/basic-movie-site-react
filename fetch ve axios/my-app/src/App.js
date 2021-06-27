4e423363fdb060de6d47303cf8ccc05de9b0c1b6
Ümit Yaşar TÜRK <76227146+umit-turk@users.noreply.github.com>
2021-06-27 17:42:43 +0300

    Add files via upload

---
+++ fetch ve axios/my-app/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import NewUsers from './components/NewUsers';
+/* import Users from './components/Users'; */
+
+function App() {
+  return (
+    <div className="App">
+       {/*  <Users /> */}
+       <NewUsers />
+    </div>
+  );
+}
+
+export default App;
