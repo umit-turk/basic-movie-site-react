e2d267ac3732221a207a91ef22070d36a2b1ffa8
Ümit Yaşar TÜRK <76227146+umit-turk@users.noreply.github.com>
2021-07-31 16:27:28 +0300

    Add files via upload

---
+++ pagination/pagination/src/components/Post.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Post = ({posts, loading}) => {
+    if(loading) {
+        return <h2>Loading...</h2>
+    }
+    return (
+        <div>
+            <ul>
+                {
+                    posts.map(post => (
+                        <li key={post.id}>{post.title}</li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Post
